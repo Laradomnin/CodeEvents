d2ada151487eda81c2f484b0c386fb331f6b2dbd
Laradomnin <lara.domnin@hotmail.com>
2022-09-01 18:47:56 +0200

    Add project files.

---
+++ CodeEvents/Data/Repositories/CodeEventRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeEvents.Api.Data.Repositories
+{
+    public class CodeEventRepository
+    {
+        private CodeEventsApiContext db;
+
+        public CodeEventRepository(CodeEventsApiContext db)
+        {
+            this.db = db;
+
+        }
+    }
+}
